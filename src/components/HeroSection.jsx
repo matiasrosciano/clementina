d7e92fdeb4e49b0940a2ad29436d187fb3a56c95
matiasrosciano <matiasrosciano@gmail.com>
2026-06-15 18:10:05 -0300

    logo

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -37,7 +37,7 @@ export function HeroSection() {
         <img
           src={logo}
           alt="Clementina"
-          className="mb-6 h-20 w-auto object-contain"
+          className="mb-6 h-auto w-full object-cover"
         />
 
         {/* Badge */}
